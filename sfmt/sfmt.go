6b61112f96ed6a08a75330fa6416aa2c50306a40
Sheng Liao <sheng@bcowtech.com>
2021-07-27 15:19:21 +0800

    Try to fix the error'...overflows _Ctype_ulong' on window On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   README.md
    	modified:   gosfmt_test.go
    	deleted:    sfmt/connectsfmt.c
    	deleted:    sfmt/connectsfmt.h
    	modified:   sfmt/sfmt.go

--- sfmt/sfmt.go
+++ sfmt/sfmt.go
@@ -7,14 +7,12 @@ package sfmt
 
 /*
 #cgo CFLAGS: -O3 -fomit-frame-pointer -DNDEBUG -fno-strict-aliasing -std=c99 -msse2 -DHAVE_SSE2 -DSFMT_MEXP=19937
-#include "connectsfmt.h"
-#include "SFMT.h"
-#include "stdio.h"
 #ifdef WIN32
 #define LONG long long
 #else
 #define LONG long
 #endif
+#include "SFMT.h"
 */
 import "C"
 
@@ -34,7 +32,7 @@ func New() *SFMT19937 {
 // Seed uses the given 64bit value to initialise the generator state.
 // This method is part of the rand.Source interface.
 func (sfmt *SFMT19937) Seed(seed int64) {
-	C.Seed(sfmt.csfmt, C.LONG(seed))
+	C.sfmt_init_gen_rand(sfmt.csfmt, C.uint32_t(seed))
 }
 
 // Uint64 generates a (pseudo-)random 64bit value.  The output can be
@@ -42,7 +40,7 @@ func (sfmt *SFMT19937) Seed(seed int64) {
 // distributed samples in the range 0, 1, ..., 2^64-1.  This method is
 // part of the rand.Source64 interface.
 func (sfmt *SFMT19937) Uint64() uint64 {
-	return uint64(C.Uint64(sfmt.csfmt))
+	return uint64(C.sfmt_genrand_uint64(sfmt.csfmt))
 }
 
 // Int63 generates a (pseudo-)random 63bit value.  The output can be
@@ -50,5 +48,5 @@ func (sfmt *SFMT19937) Uint64() uint64 {
 // distributed samples in the range 0, 1, ..., 2^63-1.  This method is
 // part of the rand.Source interface.
 func (sfmt *SFMT19937) Int63() int64 {
-	return int64(C.Uint64(sfmt.csfmt) & 0x7fffffffffffffff)
+	return int64(C.sfmt_genrand_uint64(sfmt.csfmt) & 0x7fffffffffffffff)
 }
